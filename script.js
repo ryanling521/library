5f166969c4572d63a3111c8f8c6662edab8b3cb7
Ryan <ryanling521@g.ucla.edu>
2022-06-28 00:06:52 -0700

    included add new book button and displays books

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 // target elements on DOM
 const body = document.body;
+const container = document.querySelector(".container");
 const author = document.querySelector("#author");
 const title = document.querySelector("#title");
 const pages = document.querySelector("#pages");
+const addBookButton = document.querySelector('.addBookButton');
+const form = document.querySelector('#form');
+
+// display for function
+function displayForm() {
+    form.style.display = "initial";
+}
+
+//shows the form on click
+addBookButton.addEventListener('click', displayForm);
 
 // adds book to myLibrary
 function addBook(book) {
+    // check if anything is blank
+    if ((author.value == "") || (title.value == "") || (pages.value == "")) {
+        return;
+    }
     myLibrary.push(book);
+    // resets form when submitted
+    form.reset();
 }
 
 // initialize empty library
 let myLibrary = [];
 
-// loops through myLibrary array
-function loopArray(array) {
-    array.forEach(element => {
-        let newDiv = document.createElement('div');
-        newDiv.textContent = element;
-        body.append(newDiv);
+// display library
+function display(array) {
+    array.forEach(object => {
+        for (let key in object) {
+            let newDiv = document.createElement('div');
+            newDiv.textContent = `${key}: ${object[key]}`;
+            container.append(newDiv); 
+        }
     });
+}
+
+// reset displays
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
 function Book(title, author, pages) { //read
@@ -38,14 +64,15 @@ function Book(title, author, pages) { //read
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    let book = new Book(title.value, author.value, pages.value)
+    addBook(book);
+    display(myLibrary);
 }
 
 // display books on scren
-
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', function() {
-    let book = new Book(title.value, author.value, pages.value)
-    console.log(book);
+    removeAllChildNodes(container);
+    addBookToLibrary();
 
 })
